6f449304cf2b8dabee63464be7b58acd54ec12f2
sebas2180 <54959701+sebas2180@users.noreply.github.com>
2020-04-02 19:34:28 -0300

    222

--- index.js
+++ index.js
@@ -7,15 +7,34 @@ const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const server = http.createServer(app);
+const flash = require('connect-flash');
+
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(flash());
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 app.set('port',PORT);
 app.use(express.static(path.join(__dirname,'public')));
+
+
 server.listen(PORT,()=>{//cambiar a 30000 en desarollo
     console.log('server conectado en el puerto: '+server.address().port)
 });
+
+
+app.options("/*", function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.sendStatus(200);
+});
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 
 const  usuarioRoute= require('./public/rutas/usuarioRoute')(app);
  require('./public/database/sequelize');
--- public/Services/usuarioService.js
+++ public/Services/usuarioService.js
@@ -5,15 +5,15 @@ module.exports={
              usuarioModel.findAll()
                .then(
                    resp=>{
-                       //console.log(resp[0].dataValues);
-                       const sendInfo={
+ 
+                    const sendInfo={
                            status: 752,
                            msj:'Banco encontrados',
-                           bancos: resp[0].dataValues
+                           bancos: resp
                          }
                          console.log(sendInfo);
                       resolve(sendInfo);
-                   }
+                    }
                )
        })
     
