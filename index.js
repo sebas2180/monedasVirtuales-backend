ebd74cb61eb3514c90ec9aea16b35a9256d2dc48
sebas2180 <54959701+sebas2180@users.noreply.github.com>
2020-04-02 15:33:33 -0300

    make it better

---
+++ index.js
@@ -0,0 +1,22 @@
+const express= require('express');
+const app = express();
+const http = require('http');
+//const cors = require('cors');
+const passport = require('passport');
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const server = http.createServer(app);
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
+app.set('port',PORT);
+app.use(express.static(path.join(__dirname,'public')));
+server.listen(PORT,()=>{//cambiar a 30000 en desarollo
+    console.log('server conectado en el puerto: '+server.address().port)
+});
+
+app.get('/',(req,res,next)=>{
+    res.send({a:'hola'});
+});
