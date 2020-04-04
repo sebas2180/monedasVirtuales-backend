0b31129996b09d1c19888c4ddc070bfefdfbd78c
sebas2180 <54959701+sebas2180@users.noreply.github.com>
2020-04-04 19:05:41 -0300

    asdf

--- database/sequelize.js
+++ database/sequelize.js
@@ -2,26 +2,24 @@ const Sequelize = require('sequelize');
 
 var sequelize ;
 var env = process.argv[2] || 'dev';
-switch (env) {
-    case 'dev':
-       sequelize = new Sequelize('crytoInfo', 'root', '1234', {
-        dialect: 'mysql',
-        host: 'localhost',
-        define: {
-          timestamps: false
-      }
-      });
-        break;
-    case 'prod':
-       sequelize = new Sequelize('heroku_9ec3058ce556a10', 'b85c9c21359917', '7f820141', {
+
+    // sequelize = new Sequelize('crytoInfo', 'root', '1234', {
+    //     dialect: 'mysql',
+    //     host: 'localhost',
+    //     define: {
+    //       timestamps: false
+    //   }
+    //   });
+
+    sequelize = new Sequelize('heroku_9ec3058ce556a10', 'b85c9c21359917', '7f820141', {
         dialect: 'mysql',
         host: 'us-cdbr-iron-east-01.cleardb.net',
         define: {
           timestamps: false
       }
       });
-        break;
-}
+
+
 
   
   sequelize.authenticate()
