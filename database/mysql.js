c7d8f5a30903782a00badc7a4f898191063dc8c0
sebas2180 <54959701+sebas2180@users.noreply.github.com>
2020-04-04 17:52:22 -0300

    asdf

--- database/mysql.js
+++ database/mysql.js
@@ -12,27 +12,27 @@ hola:  function(){
 
 dbConnection: function () {
 
-    // connection = mysql.createConnection({
-    //     host: 'us-cdbr-iron-east-01.cleardb.net',
-    //     user: 'bb4140aeaa24a0',
-    //     password: 'af327668',
-    //     // database: 'heroku_3f3fe830ae5ac81',
-    //     insecureAuth : true,
-    //     port: 3306,
-    //     dateStrings:true
-    // });
-
-
-    console.log('a');
     connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '1234',
-        database: 'crytoinfo',
+        host: 'us-cdbr-iron-east-01.cleardb.net',
+        user: 'bb4140aeaa24a0',
+        password: 'af327668',
+        // database: 'heroku_3f3fe830ae5ac81',
         insecureAuth : true,
         port: 3306,
         dateStrings:true
     });
+
+
+    // console.log('a');
+    // connection = mysql.createConnection({
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: '1234',
+    //     database: 'crytoinfo',
+    //     insecureAuth : true,
+    //     port: 3306,
+    //     dateStrings:true
+    // });
 
     console.log('conexion a BD exitosa');
     connection.connect();
