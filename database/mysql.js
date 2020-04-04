216995845020ddaa95622f7318de509fc13e6bc2
sebas2180 <54959701+sebas2180@users.noreply.github.com>
2020-04-04 19:40:00 -0300

    asdf

--- database/mysql.js
+++ database/mysql.js
@@ -29,25 +29,6 @@ module.exports = {
  
 dbConnection: function () {
 
-    // connection = mysql.createConnection(db_config);
-
-    // console.log('conexion a BD exitosa.');
-    // connection.connect(function(err) {              // The server is either down
-    //     if(err) {                                     // or restarting (takes a while sometimes).
-    //       console.log('error when connecting to db:', err);
-    //       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    //     }                                     // to avoid a hot loop, and to allow our node script to
-    //   });
-
-    //   connection.on('error', function(err) {
-    //     console.log('db error', err);
-    //     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-    //       handleDisconnect();                         // lost due to either server restart, or a
-    //     } else {                                      // connnection idle timeout (the wait_timeout
-    //       throw err;                                  // server variable configures this)
-    //     }
-    //   });
-
   connection = mysql.createConnection(db_config); // Recreate the connection, since
                                                   // the old one cannot be reused.
 
